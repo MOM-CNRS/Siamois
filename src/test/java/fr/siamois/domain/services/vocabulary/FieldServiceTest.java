b639023d08007835eaca49a529f2d64ce3ae6f49
Julien Linget <julien.linget@mom.fr>
2025-10-28 13:57:04 +0100

    test: FieldServiceTest.java

--- src/test/java/fr/siamois/domain/services/vocabulary/FieldServiceTest.java
+++ src/test/java/fr/siamois/domain/services/vocabulary/FieldServiceTest.java
@@ -1,5 +1,6 @@
 package fr.siamois.domain.services.vocabulary;
 
+import fr.siamois.domain.models.FieldCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +25,22 @@ class FieldServiceTest {
         List<String> fieldCodes = fieldService.searchAllFieldCodes();
 
         assertThat(fieldCodes).isNotEmpty();
+    }
+
+    @Test
+    void findFieldCodesOf_shouldReturnFieldCodes_ofClass() {
+        List<String> results = fieldService.findFieldCodesOf(TestClass.class);
+        assertThat(results).containsExactlyInAnyOrder("CODE1", "CODE2");
+    }
+
+    private static class TestClass {
+        @FieldCode
+        public static final String FIELD_CODE_1 = "code1";
+
+        @FieldCode
+        public static final String FIELD_CODE_2 = "code2";
+
+        private final String NOT_A_FIELD_CODE = "notAFieldCode";
     }
 
 }
