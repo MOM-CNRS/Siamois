6bff355014e81fb624dae29b90993bb317329259
Julien Linget <julien.linget@mom.fr>
2025-10-28 17:19:51 +0100

    fix: Use Long as primary key

--- src/main/java/fr/siamois/domain/models/vocabulary/ConceptHierarchy.java
+++ src/main/java/fr/siamois/domain/models/vocabulary/ConceptHierarchy.java
@@ -5,66 +5,37 @@ import lombok.*;
 
 @Getter
 @Entity
+@Setter
 @Table(name = "concept_hierarchy")
 public class ConceptHierarchy {
 
-    @EmbeddedId
-    private ConceptRelationId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "concept_hierarchy_id", nullable = false)
+    private Long id;
 
-    @MapsId("parentId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "fk_parent_concept_id")
     private Concept parent;
 
-    @MapsId("childId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "fk_child_concept_id")
     private Concept child;
 
-    @MapsId("parentFieldContextId")
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fk_parent_field_context_id")
     private Concept parentFieldContext;
 
     public ConceptHierarchy(Concept parent, Concept child, Concept parentFieldContext) {
-        id = new ConceptRelationId(parent.getId(), child.getId(), parentFieldContext.getId());
         this.parent = parent;
         this.child = child;
         this.parentFieldContext = parentFieldContext;
     }
 
     public ConceptHierarchy() {
-        id = new ConceptRelationId();
         parent = null;
         child = null;
         parentFieldContext = null;
-    }
-
-    public void setChild(Concept child) {
-        this.child = child;
-        id.childId = child.getId();
-    }
-
-    public void setParent(Concept parent) {
-        this.parent = parent;
-        id.parentId = parent.getId();
-    }
-
-    public void setParentFieldContext(Concept parentFieldContext) {
-        this.parentFieldContext = parentFieldContext;
-        id.parentFieldContextId = parentFieldContext.getId();
-    }
-
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Embeddable
-    @EqualsAndHashCode
-    @Getter
-    @Setter
-    public static class ConceptRelationId {
-        private long parentId;
-        private long childId;
-        private long parentFieldContextId;
     }
 
 }
--- src/main/java/fr/siamois/infrastructure/database/repositories/vocabulary/ConceptRelationRepository.java
+++ src/main/java/fr/siamois/infrastructure/database/repositories/vocabulary/ConceptRelationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConceptRelationRepository extends CrudRepository<ConceptHierarchy, ConceptHierarchy.ConceptRelationId> {
+public interface ConceptRelationRepository extends CrudRepository<ConceptHierarchy, Long> {
 
 }
