8fd1e15e5b60f72789dfbe1e62b2e5260d83def8
Julien Linget <julien.linget@mom.fr>
2025-10-28 13:53:26 +0100

    test: FieldConfigurationService

--- src/test/java/fr/siamois/domain/services/vocabulary/FieldConfigurationServiceTest.java
+++ src/test/java/fr/siamois/domain/services/vocabulary/FieldConfigurationServiceTest.java
@@ -4,8 +4,10 @@ import fr.siamois.domain.models.UserInfo;
 import fr.siamois.domain.models.auth.Person;
 import fr.siamois.domain.models.exceptions.ErrorProcessingExpansionException;
 import fr.siamois.domain.models.exceptions.api.NotSiamoisThesaurusException;
+import fr.siamois.domain.models.exceptions.vocabulary.NoConfigForFieldException;
 import fr.siamois.domain.models.institution.Institution;
 import fr.siamois.domain.models.settings.ConceptFieldConfig;
+import fr.siamois.domain.models.spatialunit.SpatialUnit;
 import fr.siamois.domain.models.vocabulary.Concept;
 import fr.siamois.domain.models.vocabulary.FeedbackFieldConfig;
 import fr.siamois.domain.models.vocabulary.Vocabulary;
@@ -27,6 +29,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -66,6 +69,7 @@ class FieldConfigurationServiceTest {
         vocabulary.setId(-1L);
         vocabulary.setType(type);
         vocabulary.setExternalVocabularyId("th2");
+        vocabulary.setBaseUri("http://exemple.org");
 
         userInfo = new UserInfo(new Institution(), new Person(), "fr");
         userInfo.getInstitution().setId(12L);
@@ -118,13 +122,195 @@ class FieldConfigurationServiceTest {
     @Test
     void setupFieldConfigurationForInstitution_shouldThrowNotSiamoisThesaurusException_whenSIAAUTOCOMPLETEmissing() throws NotSiamoisThesaurusException, ErrorProcessingExpansionException {
         when(conceptApi.fetchFieldsBranch(vocabulary)).thenThrow(NotSiamoisThesaurusException.class);
-        Assertions.assertThrows(NotSiamoisThesaurusException.class, () -> service.setupFieldConfigurationForInstitution(userInfo, vocabulary));
+        assertThrows(NotSiamoisThesaurusException.class, () -> service.setupFieldConfigurationForInstitution(userInfo, vocabulary));
     }
 
     @Test
     void setupFieldConfigurationForInstitution_shouldThrowErrorProcessingExpansionException_whenResponseIsInvalid() throws NotSiamoisThesaurusException, ErrorProcessingExpansionException {
         when(conceptApi.fetchFieldsBranch(vocabulary)).thenThrow(ErrorProcessingExpansionException.class);
-        Assertions.assertThrows(ErrorProcessingExpansionException.class, () -> service.setupFieldConfigurationForInstitution(userInfo, vocabulary));
+        assertThrows(ErrorProcessingExpansionException.class, () -> service.setupFieldConfigurationForInstitution(userInfo, vocabulary));
+    }
+
+    @Test
+    void setupFieldConfigurationForUser_shouldReturnFieldConfigIfWrong_whenMissingFieldCodes() throws NotSiamoisThesaurusException, ErrorProcessingExpansionException {
+        when(conceptApi.fetchFieldsBranch(vocabulary)).thenReturn(conceptBranchDTO);
+        when(fieldService.searchAllFieldCodes()).thenReturn(List.of("SIATEST", "SIATEST2", "SIATEST3"));
+
+        Optional<FeedbackFieldConfig> result = service.setupFieldConfigurationForUser(userInfo, vocabulary);
+
+        assertThat(result).isPresent();
+    }
+
+    @Test
+    void setupFieldConfigurationForUser_shouldReturnEmptyWhenValid_andConfigDoesNotExist() throws NotSiamoisThesaurusException, ErrorProcessingExpansionException {
+        when(conceptApi.fetchFieldsBranch(vocabulary)).thenReturn(conceptBranchDTO);
+        when(fieldService.searchAllFieldCodes()).thenReturn(List.of("SIATEST", "SIATEST2"));
+        doAnswer(i -> {
+            Vocabulary vocab = i.getArgument(0);
+            FullInfoDTO dto = i.getArgument(1);
+            Concept concept = new Concept();
+            concept.setVocabulary(vocab);
+            concept.setExternalId(dto.getIdentifier()[0].getValue());
+            return concept;
+        }).when(conceptService).saveOrGetConceptFromFullDTO(any(Vocabulary.class), any(FullInfoDTO.class), eq(null));
+        when(conceptFieldConfigRepository.findByFieldCodeForUser(eq(userInfo.getUser().getId()), anyString())).thenReturn(Optional.empty());
+        when(conceptFieldConfigRepository.save(any(ConceptFieldConfig.class))).thenAnswer(i -> i.getArgument(0));
+
+        Optional<FeedbackFieldConfig> result = service.setupFieldConfigurationForUser(userInfo, vocabulary);
+
+        assertThat(result).isEmpty();
+        verify(conceptFieldConfigRepository, times(2)).save(any(ConceptFieldConfig.class));
+        verify(conceptService, times(2)).saveAllSubConceptOfIfUpdated(any(ConceptFieldConfig.class));
+    }
+
+    @Test
+    void setupFieldConfigurationForUser_shouldThrowNotSiamoisThesaurusException_whenSIAAUTOCOMPLETEmissing() throws NotSiamoisThesaurusException, ErrorProcessingExpansionException {
+        when(conceptApi.fetchFieldsBranch(vocabulary)).thenThrow(NotSiamoisThesaurusException.class);
+        assertThrows(NotSiamoisThesaurusException.class, () -> service.setupFieldConfigurationForUser(userInfo, vocabulary));
+    }
+
+    @Test
+    void setupFieldConfigurationForUser_shouldThrowErrorProcessingExpansionException_whenResponseIsInvalid() throws NotSiamoisThesaurusException, ErrorProcessingExpansionException {
+        when(conceptApi.fetchFieldsBranch(vocabulary)).thenThrow(ErrorProcessingExpansionException.class);
+        assertThrows(ErrorProcessingExpansionException.class, () -> service.setupFieldConfigurationForUser(userInfo, vocabulary));
+    }
+
+    @Test
+    void findVocabularyUrlOfInstitution_shouldReturnString_whenConfigExists() {
+        Concept c = new Concept();
+        c.setVocabulary(vocabulary);
+        c.setExternalId("12");
+        when(conceptRepository.findTopTermConfigForFieldCodeOfInstitution(userInfo.getInstitution().getId(), SpatialUnit.CATEGORY_FIELD_CODE))
+                .thenReturn(Optional.of(c));
+        Optional<String> result = service.findVocabularyUrlOfInstitution(userInfo.getInstitution());
+
+        assertThat(result).isPresent()
+                .get()
+                .isEqualTo("http://exemple.org/?idt=th2");
+
+    }
+
+    @Test
+    void findVocabularyUrlOfInstitution_shouldReturnEmpty_whenConfigDoesNotExist() {
+        when(conceptRepository.findTopTermConfigForFieldCodeOfInstitution(userInfo.getInstitution().getId(), SpatialUnit.CATEGORY_FIELD_CODE))
+                .thenReturn(Optional.empty());
+        Optional<String> result = service.findVocabularyUrlOfInstitution(userInfo.getInstitution());
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void findParentConceptForFieldcode_shouldReturnConcept_whenConfigExists() throws NoConfigForFieldException {
+        ConceptFieldConfig cfc = new ConceptFieldConfig();
+        Concept concept = new Concept();
+        concept.setVocabulary(vocabulary);
+        concept.setExternalId("12");
+        cfc.setConcept(concept);
+        cfc.setFieldCode(SpatialUnit.CATEGORY_FIELD_CODE);
+
+        when(conceptFieldConfigRepository.findByFieldCodeForInstitution(userInfo.getInstitution().getId(), SpatialUnit.CATEGORY_FIELD_CODE))
+                .thenReturn(Optional.of(cfc));
+
+        Concept result = service.findParentConceptForFieldcode(userInfo, SpatialUnit.CATEGORY_FIELD_CODE);
+
+        assertThat(result)
+                .isNotNull()
+                .isEqualTo(concept);
+    }
+
+    @Test
+    void findParentConceptForFieldcode_shouldThrowNoConfigException_whenConfigDoesNotExist() {
+        assertThrows(NoConfigForFieldException.class, () -> service.findParentConceptForFieldcode(userInfo, SpatialUnit.CATEGORY_FIELD_CODE));
+    }
+
+    @Test
+    void getUrlOfConcept_shouldReturnUrl_whenConceptIsValid() {
+        Concept concept = new Concept();
+        concept.setVocabulary(vocabulary);
+        concept.setExternalId("12345");
+
+        String result = service.getUrlOfConcept(concept);
+
+        assertThat(result).isEqualTo("http://exemple.org/?idc=12345&idt=th2");
+    }
+
+    @Test
+    void getUrlForFieldCode_shouldReturnUrl_whenConfigExists() {
+        ConceptFieldConfig cfc = new ConceptFieldConfig();
+        Concept concept = new Concept();
+        concept.setVocabulary(vocabulary);
+        concept.setExternalId("12");
+        cfc.setConcept(concept);
+        cfc.setFieldCode(SpatialUnit.CATEGORY_FIELD_CODE);
+
+        when(conceptFieldConfigRepository.findByFieldCodeForInstitution(userInfo.getInstitution().getId(), SpatialUnit.CATEGORY_FIELD_CODE))
+                .thenReturn(Optional.of(cfc));
+
+        String result = service.getUrlForFieldCode(userInfo, SpatialUnit.CATEGORY_FIELD_CODE);
+
+        assertThat(result).isEqualTo("http://exemple.org/?idc=12&idt=th2");
+    }
+
+    @Test
+    void getUrlForFieldCode_shouldReturnNull_whenConfigDoesNotExist() {
+        String result = service.getUrlForFieldCode(userInfo, SpatialUnit.CATEGORY_FIELD_CODE);
+
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void findConfigurationForFieldCode_shouldReturnConfig_whenExists() throws NoConfigForFieldException {
+        ConceptFieldConfig cfc = new ConceptFieldConfig();
+        Concept concept = new Concept();
+        concept.setVocabulary(vocabulary);
+        concept.setExternalId("12");
+        cfc.setConcept(concept);
+        cfc.setFieldCode(SpatialUnit.CATEGORY_FIELD_CODE);
+
+        when(conceptFieldConfigRepository.findByFieldCodeForInstitution(userInfo.getInstitution().getId(), SpatialUnit.CATEGORY_FIELD_CODE))
+                .thenReturn(Optional.of(cfc));
+
+        ConceptFieldConfig result = service.findConfigurationForFieldCode(userInfo, SpatialUnit.CATEGORY_FIELD_CODE);
+
+        assertThat(result)
+                .isEqualTo(cfc);
+    }
+
+    @Test
+    void findConfigurationForFieldCode_shouldThrowNoConfigException_whenConfigDoesNotExist() {
+        assertThrows(NoConfigForFieldException.class, () -> service.findConfigurationForFieldCode(userInfo, SpatialUnit.CATEGORY_FIELD_CODE));
+    }
+
+    @Test
+    void fetchAutocomplete() throws NoConfigForFieldException {
+        Concept concept = new Concept();
+        concept.setVocabulary(vocabulary);
+
+        String fieldCode = "TESTFIELD";
+        String query = "test query";
+
+        ConceptFieldConfig cfc = new ConceptFieldConfig();
+        cfc.setConcept(concept);
+        cfc.setFieldCode(fieldCode);
+
+        when(conceptFieldConfigRepository.findByFieldCodeForInstitution(userInfo.getInstitution().getId(), fieldCode))
+                .thenReturn(Optional.of(cfc));
+        when(labelService.findMatchingConcepts(concept, userInfo.getLang(), query, FieldConfigurationService.LIMIT_RESULTS)).thenReturn(List.of(concept));
+
+        List<Concept> result = service.fetchAutocomplete(userInfo, fieldCode, query);
+
+        assertThat(result)
+                .isNotNull()
+                .hasSize(1)
+                .containsExactly(concept);
+    }
+
+    @Test
+    void fetchAutocomplete_shouldThrowNoConfigException_whenConfigDoesNotExist() {
+        String fieldCode = "TESTFIELD";
+        String query = "test query";
+
+        assertThrows(NoConfigForFieldException.class, () -> service.fetchAutocomplete(userInfo, fieldCode, query));
     }
 
 }
