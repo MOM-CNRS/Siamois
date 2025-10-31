0708edb801be238a91d01c25ac94db2aa2653a94
Julien Linget <julien.linget@mom.fr>
2025-10-31 17:27:16 +0100

    WIP: Error while SAVING ConceptLabel

--- src/main/java/fr/siamois/ui/bean/converter/ConceptLabelConverter.java
+++ src/main/java/fr/siamois/ui/bean/converter/ConceptLabelConverter.java
@@ -3,34 +3,35 @@ package fr.siamois.ui.bean.converter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.siamois.domain.models.vocabulary.label.ConceptLabel;
+import fr.siamois.ui.bean.LabelBean;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.convert.Converter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Converter for ConceptLabel objects. Uses the LabelBean cache to convert between ConceptLabel objects and their string representations.
+ */
+@SessionScoped
 @Component
+@RequiredArgsConstructor
 public class ConceptLabelConverter implements Converter<ConceptLabel> {
 
-    private final ObjectMapper objectMapper;
-
-    public ConceptLabelConverter(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    private final LabelBean labelBean;
 
     @Override
     public ConceptLabel getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
-        if (s == null || s.isEmpty()) {
-            return null;
-        }
-        return objectMapper.convertValue(s, ConceptLabel.class);
+        return labelBean.findById(Long.parseLong(s)).orElse(null);
     }
 
     @Override
     public String getAsString(FacesContext facesContext, UIComponent uiComponent, ConceptLabel conceptLabel) {
-        try {
-            return objectMapper.writeValueAsString(conceptLabel);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+        if (conceptLabel == null) {
+            return "";
         }
+        return String.valueOf(conceptLabel.getId());
     }
 }
