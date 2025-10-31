73e7369492ab280e7ae2e8f51b0c06549f892e96
Julien Linget <julien.linget@mom.fr>
2025-10-31 10:35:12 +0100

    refactor: Optimize imports

--- src/main/java/fr/siamois/domain/services/vocabulary/LabelService.java
+++ src/main/java/fr/siamois/domain/services/vocabulary/LabelService.java
@@ -15,7 +15,9 @@ import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Service to manage labels for concepts and vocabularies.
