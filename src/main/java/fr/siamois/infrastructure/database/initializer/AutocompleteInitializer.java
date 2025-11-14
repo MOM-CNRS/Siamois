a8e616d3b7794f1470a1feb06dfe10cce03c81e0
gregory bliault <gregorybliault@gmail.com>
2025-11-14 15:23:59 +0100

    hotfix

--- src/main/java/fr/siamois/infrastructure/database/initializer/AutocompleteInitializer.java
+++ src/main/java/fr/siamois/infrastructure/database/initializer/AutocompleteInitializer.java
@@ -4,10 +4,14 @@ import com.zaxxer.hikari.HikariDataSource;
 import fr.siamois.domain.models.exceptions.database.DatabaseDataInitException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.Statement;
@@ -25,10 +29,15 @@ public class AutocompleteInitializer implements DatabaseInitializer {
     @Override
     public void initialize() throws DatabaseDataInitException {
         try {
-            File autocomplete = ResourceUtils.getFile("classpath:pgplsql/concept_autocomplete.sql");
-            String sql = Files.readString(autocomplete.toPath());
-            try (Connection connection = hikariDataSource.getConnection()) {
-                Statement statement = connection.createStatement();
+            Resource resource = new ClassPathResource("pgplsql/concept_autocomplete.sql");
+
+            String sql;
+            try (InputStream in = resource.getInputStream()) {
+                sql = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+            }
+
+            try (Connection connection = hikariDataSource.getConnection();
+                 Statement statement = connection.createStatement()) {
                 statement.executeLargeUpdate(sql);
                 log.info("Autocomplete functions initialized successfully.");
             }
