834123acf7369bf3fe6a212866f2098dbdfd23dc
Julien Linget <julien.linget@mom.fr>
2026-01-12 16:05:24 +0100

    refactor: Moved action unit form to specific classes

---
+++ src/main/java/fr/siamois/domain/models/actionunit/form/ActionUnitDetailsForm.java
@@ -0,0 +1,79 @@
+package fr.siamois.domain.models.actionunit.form;
+
+import fr.siamois.domain.models.form.customform.CustomCol;
+import fr.siamois.domain.models.form.customform.CustomForm;
+import fr.siamois.domain.models.form.customform.CustomFormPanel;
+import fr.siamois.domain.models.form.customform.CustomRow;
+
+import static fr.siamois.ui.bean.panel.models.panel.single.AbstractSingleEntity.COLUMN_CLASS_NAME;
+
+public class ActionUnitDetailsForm extends ActionUnitForm {
+
+    public static CustomForm build() {
+        return new CustomForm.Builder()
+                .name(DETAIL_TAB_NAME)
+                .description("")
+                .addPanel(
+                        new CustomFormPanel.Builder()
+                                .name(GENERAL_LABEL_CODE)
+                                .isSystemPanel(true)
+                                .addRow(
+                                        new CustomRow.Builder()
+                                                .addColumn(new CustomCol.Builder()
+                                                        .readOnly(false)
+                                                        .className(COLUMN_CLASS_NAME)
+                                                        .field(NAME_FIELD)
+                                                        .build())
+                                                .addColumn(new CustomCol.Builder()
+                                                        .readOnly(true)
+                                                        .className(COLUMN_CLASS_NAME)
+                                                        .field(IDENTIFIER_FIELD)
+                                                        .isRequired(true)
+                                                        .build())
+                                                .addColumn(new CustomCol.Builder()
+                                                        .readOnly(false)
+                                                        .className(COLUMN_CLASS_NAME)
+                                                        .field(ACTION_UNIT_TYPE_FIELD)
+                                                        .build())
+                                                .addColumn(new CustomCol.Builder()
+                                                        .readOnly(false)
+                                                        .className(COLUMN_CLASS_NAME)
+                                                        .field(ACTION_CODE_FIELD)
+                                                        .build())
+                                                .build()
+                                )
+                                .addRow(
+                                        new CustomRow.Builder()
+                                                .addColumn(new CustomCol.Builder()
+                                                        .readOnly(false)
+                                                        .className(COLUMN_CLASS_NAME)
+                                                        .field(BEGIN_DATE_FIELD)
+                                                        .isRequired(false)
+                                                        .build())
+                                                .addColumn(new CustomCol.Builder()
+                                                        .readOnly(false)
+                                                        .className(COLUMN_CLASS_NAME)
+                                                        .field(END_DATE_FIELD)
+                                                        .isRequired(false)
+                                                        .build())
+                                                .build()
+                                ).build()
+                )
+                .addPanel(
+                        new CustomFormPanel.Builder()
+                                .name(SPATIAL_UNIT_CONTEXT_LABEL_CODE)
+                                .isSystemPanel(true)
+                                .addRow(
+                                        new CustomRow.Builder()
+                                                .addColumn(new CustomCol.Builder()
+                                                        .readOnly(false)
+                                                        .className("ui-g-12 ui-md-12 ui-lg-12")
+                                                        .field(SPATIAL_CONTEXT_FIELD)
+                                                        .build())
+                                                .build()
+                                ).build()
+                )
+                .build();
+    }
+
+}
