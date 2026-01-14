6aa1537e526f52e117c04c4e0cbd46a55c020ed1
Julien Linget <julien.linget@mom.fr>
2026-01-14 12:55:39 +0100

    feat: Refactor Action Unit form

--- src/main/java/fr/siamois/domain/models/actionunit/ActionUnit.java
+++ src/main/java/fr/siamois/domain/models/actionunit/ActionUnit.java
@@ -3,6 +3,8 @@ package fr.siamois.domain.models.actionunit;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import fr.siamois.domain.models.ArkEntity;
 import fr.siamois.domain.models.FieldCode;
+import fr.siamois.domain.models.actionunit.form.ActionUnitDetailsForm;
+import fr.siamois.domain.models.actionunit.form.ActionUnitNewForm;
 import fr.siamois.domain.models.document.Document;
 import fr.siamois.domain.models.exceptions.institution.NullInstitutionIdentifier;
 import fr.siamois.domain.models.form.customfield.*;
@@ -16,6 +18,7 @@ import fr.siamois.domain.models.vocabulary.Concept;
 import fr.siamois.ui.bean.panel.models.panel.single.AbstractSingleEntity;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
 
 import java.util.HashSet;
@@ -29,14 +32,8 @@ import static fr.siamois.ui.bean.panel.models.panel.single.AbstractSingleEntity.
 @Entity
 @Table(name = "action_unit", uniqueConstraints = @UniqueConstraint(columnNames = "identifier"))
 @Audited
+@NoArgsConstructor
 public class ActionUnit extends ActionUnitParent implements ArkEntity {
-
-    private static final String SPATIAL_UNIT_CONTEXT_LABEL_CODE = "common.label.spatialContext";
-    private static final String GENERAL_LABEL_CODE = "common.header.general";
-    private static final String DETAIL_TAB_NAME = "\"Details tab form\"";
-
-    public ActionUnit() {
-    }
 
     public ActionUnit(ActionUnit unit) {
         this.setName(unit.getName());
@@ -123,258 +120,18 @@ public class ActionUnit extends ActionUnitParent implements ArkEntity {
 
     @Transient
     @JsonIgnore
+    @SuppressWarnings("unused")
     public List<String> getBindableFieldNames() {
         return List.of("type", "name", "identifier", "spatialContext", "beginDate", "endDate", "primaryActionCode");
     }
 
-// ----------- Concepts for system fields
+    @Transient
+    @JsonIgnore
+    public static final CustomForm NEW_UNIT_FORM = ActionUnitNewForm.build();
 
 
     @Transient
     @JsonIgnore
-    public static final Concept ACTION_UNIT_TYPE_CONCEPT = new Concept.Builder()
-            .vocabulary(SYSTEM_THESO)
-            .externalId("4282386")
-            .build();
-
-    // unit name
-    @Transient
-    @JsonIgnore
-    public static final Concept NAME_CONCEPT = new Concept.Builder()
-            .vocabulary(SYSTEM_THESO)
-            .externalId("4285848")
-            .build();
-
-    // unit id
-    @Transient
-    @JsonIgnore
-    public static final Concept IDENTIFIER_CONCEPT = new Concept.Builder()
-            .vocabulary(SYSTEM_THESO)
-            .externalId("4286368")
-            .build();
-
-
-    // spatial context
-    @Transient
-    @JsonIgnore
-    public static final Concept SPATIAL_CONTEXT_CONCEPT = new Concept.Builder()
-            .vocabulary(SYSTEM_THESO)
-            .externalId("4286503")
-            .build();
-
-    // begin date
-    @Transient
-    @JsonIgnore
-    public static final Concept BEGIN_DATE_CONCEPT = new Concept.Builder()
-            .vocabulary(SYSTEM_THESO)
-            .externalId("4287545")
-            .build();
-
-    // end date
-    @Transient
-    @JsonIgnore
-    public static final Concept END_DATE_CONCEPT = new Concept.Builder()
-            .vocabulary(SYSTEM_THESO)
-            .externalId("4287546")
-            .build();
-
-    // end date
-    @Transient
-    @JsonIgnore
-    public static final Concept ACTION_CODE_CONCEPT = new Concept.Builder()
-            .vocabulary(SYSTEM_THESO)
-            .externalId("4287547")
-            .build();
-
-
-    // --------------- Fields
-    @Transient
-    @JsonIgnore
-    public static final CustomFieldSelectOneFromFieldCode ACTION_UNIT_TYPE_FIELD = CustomFieldSelectOneFromFieldCode.builder()
-            .label("specimen.field.category")
-            .isSystemField(true)
-            .id(4L)
-            .valueBinding("type")
-            .styleClass("mr-2 action-unit-type-chip")
-            .iconClass("bi bi-bucket")
-            .fieldCode(ActionUnit.TYPE_FIELD_CODE)
-            .concept(ACTION_UNIT_TYPE_CONCEPT)
-            .build();
-
-    @Transient
-    @JsonIgnore
-    public static final CustomFieldText NAME_FIELD = CustomFieldText.builder()
-            .label("common.label.name")
-            .isSystemField(true)
-            .id(2L)
-            .valueBinding("name")
-            .concept(NAME_CONCEPT)
-            .build();
-
-    @Transient
-    @JsonIgnore
-    public static final CustomFieldText IDENTIFIER_FIELD = CustomFieldText.builder()
-            .label("common.label.identifier")
-            .id(1L)
-            .isSystemField(true)
-            .autoGenerationFunction(AbstractSingleEntity::generateRandomActionUnitIdentifier)
-            .valueBinding("identifier")
-            .concept(IDENTIFIER_CONCEPT)
-            .build();
-
-    @Transient
-    @JsonIgnore
-    public static final CustomFieldSelectMultipleSpatialUnitTree SPATIAL_CONTEXT_FIELD = CustomFieldSelectMultipleSpatialUnitTree.builder()
-            .label("common.label.selectedSpatialUnits")
-            .isSystemField(true)
-            .id(3L)
-            .valueBinding("spatialContext")
-            .concept(SPATIAL_CONTEXT_CONCEPT)
-            .build();
-
-    @Transient
-    @JsonIgnore
-    private static final CustomFieldDateTime BEGIN_DATE_FIELD =  CustomFieldDateTime.builder()
-            .label("common.field.beginDate")
-            .isSystemField(true)
-            .valueBinding("beginDate")
-            .showTime(false)
-            .concept(BEGIN_DATE_CONCEPT)
-            .build();
-
-    @Transient
-    @JsonIgnore
-    private static final CustomFieldDateTime END_DATE_FIELD =  CustomFieldDateTime.builder()
-            .label("common.field.endDate")
-            .isSystemField(true)
-            .valueBinding("endDate")
-            .showTime(false)
-            .concept(END_DATE_CONCEPT)
-            .build();
-
-    @Transient
-    @JsonIgnore
-    private static final CustomFieldSelectOneActionCode ACTION_CODE_FIELD = CustomFieldSelectOneActionCode.builder()
-            .label("actionunit.field.actionCode")
-            .isSystemField(true)
-            .valueBinding("primaryActionCode")
-            .concept(ACTION_CODE_CONCEPT)
-            .build();
-
-    @Transient
-    @JsonIgnore
-    public static final CustomForm NEW_UNIT_FORM = new CustomForm.Builder()
-            .name(DETAIL_TAB_NAME)
-            .description("")
-            .addPanel(
-                    new CustomFormPanel.Builder()
-                            .name(SPATIAL_UNIT_CONTEXT_LABEL_CODE)
-                            .isSystemPanel(true)
-                            .addRow(
-                                    new CustomRow.Builder()
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className("ui-g-12")
-                                                    .field(SPATIAL_CONTEXT_FIELD)
-                                                    .build())
-                                            .build()
-                            ).build()
-            )
-            .addPanel(
-                    new CustomFormPanel.Builder()
-                            .name(GENERAL_LABEL_CODE)
-                            .isSystemPanel(true)
-                            .addRow(
-                                    new CustomRow.Builder()
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .isRequired(true)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(NAME_FIELD)
-                                                    .build())
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(IDENTIFIER_FIELD)
-                                                    .isRequired(true)
-                                                    .build())
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .isRequired(true)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(ACTION_UNIT_TYPE_FIELD)
-                                                    .build())
-                                            .build()
-                            ).build()
-            )
-            .build();
-
-
-    @Transient
-    @JsonIgnore
-    public static final CustomForm DETAILS_FORM = new CustomForm.Builder()
-            .name(DETAIL_TAB_NAME)
-            .description("")
-            .addPanel(
-                    new CustomFormPanel.Builder()
-                            .name(GENERAL_LABEL_CODE)
-                            .isSystemPanel(true)
-                            .addRow(
-                                    new CustomRow.Builder()
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(NAME_FIELD)
-                                                    .build())
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(true)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(IDENTIFIER_FIELD)
-                                                    .isRequired(true)
-                                                    .build())
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(ACTION_UNIT_TYPE_FIELD)
-                                                    .build())
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(ACTION_CODE_FIELD)
-                                                    .build())
-                                            .build()
-                            )
-                            .addRow(
-                                    new CustomRow.Builder()
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(BEGIN_DATE_FIELD)
-                                                    .isRequired(false)
-                                                    .build())
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className(COLUMN_CLASS_NAME)
-                                                    .field(END_DATE_FIELD)
-                                                    .isRequired(false)
-                                                    .build())
-                                            .build()
-                            ).build()
-            )
-            .addPanel(
-                    new CustomFormPanel.Builder()
-                            .name(SPATIAL_UNIT_CONTEXT_LABEL_CODE)
-                            .isSystemPanel(true)
-                            .addRow(
-                                    new CustomRow.Builder()
-                                            .addColumn(new CustomCol.Builder()
-                                                    .readOnly(false)
-                                                    .className("ui-g-12 ui-md-12 ui-lg-12")
-                                                    .field(SPATIAL_CONTEXT_FIELD)
-                                                    .build())
-                                            .build()
-                            ).build()
-            )
-            .build();
+    public static final CustomForm DETAILS_FORM = ActionUnitDetailsForm.build();
 
 }
