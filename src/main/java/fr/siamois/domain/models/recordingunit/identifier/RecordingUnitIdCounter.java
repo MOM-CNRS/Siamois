b9332dc1adbbac166c1299c599709c1d83965d19
Julien Linget <julien.linget@mom.fr>
2026-01-15 09:32:21 +0100

    fix: Update Javadoc style of RecordingUnitIdCounter

--- src/main/java/fr/siamois/domain/models/recordingunit/identifier/RecordingUnitIdCounter.java
+++ src/main/java/fr/siamois/domain/models/recordingunit/identifier/RecordingUnitIdCounter.java
@@ -11,12 +11,12 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 /**
- * <p>Each row in this table represents a counter. A counter is associated with an ActionUnit that holds its configuration.</p>
- *
- * A counter is associated with a RecordingUnit. In this case, the counter stores the numbering for its direct children
- * of the type defined by the associated Concept. The RecordingUnit can be null, in which case it refers to
- * RecordingUnits that do not have a parent. The Concept can be null, in which case it refers to units that do not have a type
- * as it's a technical limit.
+ * Each row in this table represents a counter.
+ * A counter is associated with an ActionUnit that holds its configuration.
+ * <p>
+ * A counter is associated with a RecordingUnit. In this case, the counter stores the numbering for its direct children of the type defined by the associated Concept.
+ * The RecordingUnit can be null, in which case it refers to RecordingUnits that do not have a parent.
+ * The Concept can be null, in which case it refers to units that do not have a type as it's a technical limit.
  */
 @Data
 @Entity
