0007153294c7ee3c0eb837ab77448ba50c1a23af
Julien Linget <julien.linget@mom.fr>
2026-01-19 09:37:48 +0100

    fix: Add conditions to findAllIdentifierResolver

--- src/main/java/fr/siamois/domain/services/recordingunit/RecordingUnitService.java
+++ src/main/java/fr/siamois/domain/services/recordingunit/RecordingUnitService.java
@@ -37,6 +37,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.Modifier;
 import java.util.*;
 
 /**
@@ -528,12 +529,14 @@ public class RecordingUnitService implements ArkEntityService {
         Set<Class<? extends RuIdentifierResolver>> classes = reflections.getSubTypesOf(RuIdentifierResolver.class);
 
         for (Class<? extends RuIdentifierResolver> clazz : classes) {
-            try {
-                RuIdentifierResolver resolver = applicationContext.getBean(clazz);
-                IDENTIFIER_RESOLVERS.put(resolver.getCode(), resolver);
-            } catch (BeansException e) {
-                log.error("Error while scanning identifiers resolver of RecordingUnits");
-                log.error(e.getMessage(), e);
+            if (!clazz.isInterface() && !Modifier.isAbstract(clazz.getModifiers())) {
+                try {
+                    RuIdentifierResolver resolver = applicationContext.getBean(clazz);
+                    IDENTIFIER_RESOLVERS.put(resolver.getCode(), resolver);
+                } catch (BeansException e) {
+                    log.error("Error while scanning identifiers resolver of RecordingUnits");
+                    log.error(e.getMessage(), e);
+                }
             }
         }
 
