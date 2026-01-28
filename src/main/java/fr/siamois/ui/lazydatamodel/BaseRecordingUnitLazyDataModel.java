ae9f4c267badfa2a5c6ac68105782757a579db97
gregory bliault <gregorybliault@gmail.com>
2026-01-28 11:46:24 +0100

    Addin side panel to display relationship details

--- src/main/java/fr/siamois/ui/lazydatamodel/BaseRecordingUnitLazyDataModel.java
+++ src/main/java/fr/siamois/ui/lazydatamodel/BaseRecordingUnitLazyDataModel.java
@@ -109,7 +109,7 @@ public abstract class BaseRecordingUnitLazyDataModel extends BaseLazyDataModel<R
         RecordingUnit newRec = new RecordingUnit(original);
 
         // Save it
-        newRec = recordingUnitService.save(newRec, newRec.getType(), List.of(),  List.of(),  List.of());
+        newRec = recordingUnitService.save(newRec, newRec.getType());
 
         newRec.setFullIdentifier(recordingUnitService.generateFullIdentifier(newRec.getActionUnit(), newRec));
         if (recordingUnitService.fullIdentifierAlreadyExistInAction(newRec)) {
