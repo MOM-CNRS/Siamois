a561ef9bfe6b112b8b01248dabe694e6f8e42fe1
gregory bliault <gregorybliault@gmail.com>
2026-03-09 09:16:47 +0100

    REST API implementation

---
+++ src/main/java/fr/siamois/ui/api/openapi/v1/resource/recordingunit/RecordingUnitResource.java
@@ -0,0 +1,33 @@
+package fr.siamois.ui.api.openapi.v1.resource.recordingunit;
+
+import fr.siamois.ui.api.openapi.v1.generic.response.RelationshipCountOnly;
+import fr.siamois.ui.api.openapi.v1.generic.response.RelationshipToMany;
+import fr.siamois.ui.api.openapi.v1.generic.response.RelationshipToOne;
+import fr.siamois.ui.api.openapi.v1.resource.concept.ConceptResourceIdentifier;
+import fr.siamois.ui.api.openapi.v1.resource.person.PersonResourceIdentifier;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.OffsetDateTime;
+
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class RecordingUnitResource
+        extends RecordingUnitResourceIdentifier {
+
+    private String identifier;
+    private String fullIdentifier;
+
+    private OffsetDateTime openingDate;
+    private OffsetDateTime closingDate;
+
+    private String description;
+
+    private RelationshipToOne<ConceptResourceIdentifier> type;
+    private RelationshipToMany<PersonResourceIdentifier> contributors;
+    private RelationshipCountOnly specimen;
+
+}
