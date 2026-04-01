d3f4ed1c420586cfac798dcd398690d891bc2dd6
Julien Linget <julien.linget@mom.fr>
2026-04-01 13:03:37 +0200

    refactor: Add base API exception handling

--- src/main/java/fr/siamois/ui/api/handler/RestExceptionHandler.java
+++ src/main/java/fr/siamois/ui/api/handler/RestExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
@@ -26,6 +27,19 @@ public class RestExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ApiError> handleResponseStatusException(ResponseStatusException ex, HttpServletRequest request) {
+        ApiError error = ApiError.builder()
+                .status(ex.getStatusCode().value())
+                .error(ex.getStatusCode().toString())
+                .message(ex.getReason())
+                .path(request.getRequestURI())
+                .timestamp(OffsetDateTime.now())
+                .build();
+
+        return ResponseEntity.status(ex.getStatusCode()).body(error);
     }
 
     @ExceptionHandler(Exception.class)
