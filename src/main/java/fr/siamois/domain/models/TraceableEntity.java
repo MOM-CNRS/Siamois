1db3ece379f3656acb79cee938f491563d1455d8
gregory bliault <gregorybliault@gmail.com>
2026-04-20 16:38:50 +0200

    WIP - Measurement field in forms

--- src/main/java/fr/siamois/domain/models/TraceableEntity.java
+++ src/main/java/fr/siamois/domain/models/TraceableEntity.java
@@ -2,6 +2,7 @@ package fr.siamois.domain.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import fr.siamois.domain.models.auth.Person;
+import fr.siamois.domain.models.form.measurement.MeasurementAnswer;
 import fr.siamois.domain.models.institution.Institution;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -46,6 +47,14 @@ public abstract class TraceableEntity implements Serializable {
 
     @Column(name = "validated_at")
     protected OffsetDateTime validatedAt ;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "fk_z_sup")
+    private MeasurementAnswer zInf;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "fk_z_inf")
+    private MeasurementAnswer zSup;
 
     @ManyToOne
     @JoinColumn(name = "fk_validated_by")
