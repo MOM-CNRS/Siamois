35e62d69dc1e43b8c9535d13471178040e6753d3
gregory bliault <gregorybliault@gmail.com>
2026-04-22 17:15:13 +0200

    Adding fields on the fly

--- src/main/java/fr/siamois/ui/form/CustomFormPanelUiDto.java
+++ src/main/java/fr/siamois/ui/form/CustomFormPanelUiDto.java
@@ -11,6 +11,25 @@ public class CustomFormPanelUiDto implements Serializable {
     private String className;
     private String name;
     private List<CustomRowUiDto> rows;
+    private Boolean canUserAddFields;
     private Boolean isSystemPanel; // define by system or user
+
+    private boolean showEditor = false;
+
+    public void prepareNewField() {
+        this.showEditor = true;
+    }
+
+    public void cancelNewField() {
+        this.showEditor = false;
+    }
+
+    public void saveNewField() {
+        // 1. Create the CustomField entity
+        // 2. If it's a "Lien" type, create the Relationship Template (relTemplateId)
+        // 3. Add to the 'rows' list of the current panel
+        // 4. Reset
+        this.showEditor = false;
+    }
 
 }
