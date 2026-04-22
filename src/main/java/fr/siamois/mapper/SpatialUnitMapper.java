eef0ed921ce408b261e59a6a1349a02b13d4ce5d
Julien Linget <julien.linget@mom.fr>
2026-04-22 12:27:28 +0200

    refactor: Ignore spatial unit relations on mapper

--- src/main/java/fr/siamois/mapper/SpatialUnitMapper.java
+++ src/main/java/fr/siamois/mapper/SpatialUnitMapper.java
@@ -3,16 +3,20 @@ package fr.siamois.mapper;
 import fr.siamois.domain.models.spatialunit.SpatialUnit;
 import fr.siamois.dto.entity.SpatialUnitDTO;
 import fr.siamois.ui.mapper.adapter.ConversionServiceAdapter;
-import org.mapstruct.InheritInverseConfiguration;
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+import org.mapstruct.*;
 import org.mapstruct.extensions.spring.DelegatingConverter;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 @Mapper(uses = ConversionServiceAdapter.class, componentModel = MappingConstants.ComponentModel.SPRING,
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface SpatialUnitMapper extends Converter<SpatialUnit, SpatialUnitDTO> {
+
+    @Mapping(target = "children", ignore = true)
+    @Mapping(target = "parents", ignore = true)
+    @Mapping(target = "recordingUnitList", ignore = true)
+    @Mapping(target = "relatedActionUnitList", ignore = true)
+    SpatialUnitDTO convert(@NonNull SpatialUnit source);
 
     @InheritInverseConfiguration
     @DelegatingConverter
