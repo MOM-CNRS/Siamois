805f2f8b6ce9de665350063310b1223fb3c085c4
gregory bliault <gregorybliault@gmail.com>
2026-04-28 13:17:35 +0200

    WIP - Adding fields on the fly

--- src/main/java/fr/siamois/ui/form/NewFieldManagerBean.java
+++ src/main/java/fr/siamois/ui/form/NewFieldManagerBean.java
@@ -24,6 +24,7 @@ public class NewFieldManagerBean {
     public void prepareNewField(CustomFormPanelUiDto panel) {
         this.currentPanel = panel;
         this.showEditor = true;
+        this.newField = new CustomFieldMeasurement();
         newField.setIsSystemField(false);
     }
 
