1eae1bb7a6357f1daf9452979195f0dd17311ead
gregory bliault <gregorybliault@gmail.com>
2026-06-24 11:55:43 +0200

    Fixing tests

--- src/main/java/fr/siamois/ui/api/openapi/v1/response/document/DocumentListResponse.java
+++ src/main/java/fr/siamois/ui/api/openapi/v1/response/document/DocumentListResponse.java
@@ -1,19 +1,21 @@
 package fr.siamois.ui.api.openapi.v1.response.document;
 
-import fr.siamois.ui.api.openapi.v1.generic.response.ListMeta;
-import fr.siamois.ui.api.openapi.v1.generic.response.ListResponse;
 import fr.siamois.ui.api.openapi.v1.generic.response.Response;
 import fr.siamois.ui.api.openapi.v1.resource.document.DocumentResource;
-import fr.siamois.ui.api.openapi.v1.resource.recordingunit.RecordingUnitResource;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
 import java.util.List;
 
-@Getter
 @EqualsAndHashCode(callSuper = false)
-public class DocumentListResponse extends ListResponse<DocumentResource> {
-    public DocumentListResponse(List<DocumentResource> data, ListMeta meta) {
-        super(data, meta);
+public class DocumentListResponse extends Response<DocumentListResponse.DocumentsData> {
+
+    @Data
+    public static class DocumentsData {
+        private final List<DocumentResource> documents;
+    }
+
+    public DocumentListResponse(List<DocumentResource> documents, Object meta) {
+        super(new DocumentsData(documents));
     }
 }
