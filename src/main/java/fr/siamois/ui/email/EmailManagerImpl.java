46bc0afedb0efa47bd8ad1aa62a2e7a18225fb8d
Julien Linget <julien.linget@mom.fr>
2026-07-09 09:06:52 +0200

    feat(email): Add email manager implementation and template

---
+++ src/main/java/fr/siamois/ui/email/EmailManagerImpl.java
@@ -0,0 +1,22 @@
+package fr.siamois.ui.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailManagerImpl implements EmailManager {
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
