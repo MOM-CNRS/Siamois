67d8371af889bc00b53c9529b0d406b8be64ec2f
Grégory Bliault <gregorybliault@gmail.com>
2026-07-10 11:44:36 +0200

    Remove "Add" button from user management module (#333)
    * fix: removing add member module to user management page
    
    * fix: removing add member module to user management page

--- src/main/java/fr/siamois/ui/bean/settings/administration/ApplicationMembersListBean.java
+++ src/main/java/fr/siamois/ui/bean/settings/administration/ApplicationMembersListBean.java
@@ -8,13 +8,10 @@ import fr.siamois.dto.entity.ApplicationMemberDTO;
 import fr.siamois.dto.entity.ProfileDTO;
 import fr.siamois.ui.bean.LangBean;
 import fr.siamois.ui.bean.SessionSettingsBean;
-import fr.siamois.ui.bean.dialog.administration.NewApplicationMemberDialogBean;
-import fr.siamois.ui.bean.dialog.institution.PersonRole;
 import fr.siamois.ui.bean.settings.AbstractMembersListBean;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.primefaces.PrimeFaces;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.UnselectEvent;
 import org.springframework.context.annotation.Scope;
@@ -34,7 +31,6 @@ import static fr.siamois.utils.MessageUtils.displayWarnMessage;
 public class ApplicationMembersListBean extends AbstractMembersListBean {
 
     private final transient ApplicationMembersServiceInterface applicationMembersService;
-    private final NewApplicationMemberDialogBean newApplicationMemberDialogBean;
     private final LangBean langBean;
     private final SessionSettingsBean sessionSettingsBean;
     private final transient PersonProfileAssignmentService personProfileAssignmentService;
@@ -45,14 +41,12 @@ public class ApplicationMembersListBean extends AbstractMembersListBean {
     private String searchInput;
 
     public ApplicationMembersListBean(ApplicationMembersServiceInterface applicationMembersService,
-                                      NewApplicationMemberDialogBean newApplicationMemberDialogBean,
                                       LangBean langBean,
                                       SessionSettingsBean sessionSettingsBean,
                                       PersonProfileAssignmentService personProfileAssignmentService,
                                       PendingPersonService pendingPersonService) {
         super(pendingPersonService);
         this.applicationMembersService = applicationMembersService;
-        this.newApplicationMemberDialogBean = newApplicationMemberDialogBean;
         this.langBean = langBean;
         this.sessionSettingsBean = sessionSettingsBean;
         this.personProfileAssignmentService = personProfileAssignmentService;
@@ -64,6 +58,11 @@ public class ApplicationMembersListBean extends AbstractMembersListBean {
         members = new ArrayList<>(refMembers);
         availableProfiles = applicationMembersService.findAvailableProfiles();
         loadPendingInvitations(refMembers.stream().map(m -> m.getPerson().getId()).toList());
+    }
+
+    @Override
+    public void add() {
+        // No implementation for now. Later we might add a way to invite user to Siamois without inviting them to organization or projects.
     }
 
     /** Filters {@link #members} from {@link #refMembers} using {@link #searchInput}. */
@@ -83,16 +82,6 @@ public class ApplicationMembersListBean extends AbstractMembersListBean {
         }
     }
 
-    /** Opens the "add users" wizard dialog. */
-    @Override
-    public void add() {
-        log.trace("Creating application member");
-        newApplicationMemberDialogBean.init(langBean.msg("administrationSettings.userManagement.dialog.label"),
-                langBean.msg("organisationSettings.managers.add"),
-                this::processPerson);
-        PrimeFaces.current().ajax().update("newApplicationMemberDialog");
-        PrimeFaces.current().executeScript("PF('newApplicationMemberDialog').show();");
-    }
 
     /** Assigns the newly checked profile to the given member. */
     public void onProfileSelect(SelectEvent<ProfileDTO> event) {
@@ -119,19 +108,6 @@ public class ApplicationMembersListBean extends AbstractMembersListBean {
         }
     }
 
-    private Boolean processPerson(PersonRole saved) {
-        try {
-            ApplicationMemberDTO member = applicationMembersService.addMember(
-                    saved.person(), new ArrayList<>(saved.profiles()));
-            refMembers.add(member);
-            trackPendingInvitation(saved.person());
-            filter();
-            return true;
-        } catch (Exception err) {
-            displayWarnMessage(langBean, "administrationSettings.error.member", saved.person().getEmail());
-            return false;
-        }
-    }
 
     /** Clears the bean's state between sessions/logins. */
     @EventListener(LoginEvent.class)
