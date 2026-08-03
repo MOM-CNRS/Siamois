86996bbe6266af5b5baed49516061742be3fd8b6
Julien Linget <julien.linget@mom.fr>
2026-08-03 10:17:24 +0200

    feat(field-concept-config): Add ConceptFieldFormConfig and ConceptCollection

---
+++ src/main/java/fr/siamois/domain/models/vocabulary/ConceptCollection.java
@@ -0,0 +1,39 @@
+package fr.siamois.domain.models.vocabulary;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "concept_collection")
+@NoArgsConstructor
+public class ConceptCollection {
+
+    @NonNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "collection_id")
+    private Long id;
+
+    @NonNull
+    @Column(name = "external_id")
+    private String externalId;
+
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "fk_vocabulary_id", nullable = false)
+    private Vocabulary vocabulary;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "collection_concept",
+            joinColumns = @JoinColumn(name = "fk_collection_id"),
+            inverseJoinColumns = @JoinColumn(name = "fk_concept_id")
+    )
+    private Set<Concept> concepts = new HashSet<>();
+}
