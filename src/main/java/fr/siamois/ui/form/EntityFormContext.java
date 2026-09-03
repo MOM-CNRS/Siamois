59d761fcfa23c8f5170b4a38c82059f096c8423c
Julien Linget <julien.linget@mom.fr>
2026-09-03 09:21:31 +0200

    refactor(user-info): Ne devraient pas pouvoir être throws car elles arrêtent l'exécution du programme

--- src/main/java/fr/siamois/ui/form/EntityFormContext.java
+++ src/main/java/fr/siamois/ui/form/EntityFormContext.java
@@ -658,7 +658,7 @@ public class EntityFormContext<T extends AbstractEntityDTO> {
      */
     public List<ActionUnitSummaryDTO> completeActionUnitOptions(String query) {
         return services.getActionUnitService()
-                .findMatchingInInstitutionByName(sessionSettingsBean.getSelectedInstitution(), query, 20)
+                .findAllEditableByPersonInInstitutionByNameCompletion(query, 20)
                 .stream()
                 .map(ActionUnitSummaryDTO::new)
                 .toList();
