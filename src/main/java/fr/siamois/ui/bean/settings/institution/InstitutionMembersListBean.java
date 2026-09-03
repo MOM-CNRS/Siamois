0ff4673edb38fae9e74e708e4ea3d475e1c2d8e5
gregory bliault <gregorybliault@gmail.com>
2026-09-03 16:55:14 +0200

    fix: properly count project members

--- src/main/java/fr/siamois/ui/bean/settings/institution/InstitutionMembersListBean.java
+++ src/main/java/fr/siamois/ui/bean/settings/institution/InstitutionMembersListBean.java
@@ -189,7 +189,7 @@ public class InstitutionMembersListBean extends AbstractMembersListBean {
 
     private Boolean processPerson(PersonRole saved) {
         try {
-            if (isNotOrganisationManager(sessionSettingsBean.getSelectedInstitution())) {
+            if (isNotOrganisationManager(institution)) {
                 displayWarnMessage(langBean, SETTINGS_ERROR_NOT_MANAGER);
                 return false;
             }
